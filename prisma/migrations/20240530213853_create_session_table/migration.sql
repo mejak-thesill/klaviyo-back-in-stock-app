fc0b752fca596d902e61420b79adb0b3f00f4606
mejak-thesill <mejak@thesill.com>
2025-11-11 15:41:11 -0500

    Prisma Schema Update

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -1,5 +1,23 @@
-CREATE TABLE "Session" (
-  "id" TEXT PRIMARY KEY,
-  "data" TEXT NOT NULL,
-  "expiresAt" TIMESTAMP NOT NULL
+-- Use public schema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- ==========================
+-- Table: Session
+-- ==========================
+CREATE TABLE IF NOT EXISTS "public"."Session" (
+    "id" TEXT PRIMARY KEY,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" TIMESTAMPTZ,
+    "accessToken" TEXT,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false
 );
